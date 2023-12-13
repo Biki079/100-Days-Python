7a48b01e78d4358726f177c0a5cb24afd02ff8d0
Biki079 <bikashkc2057@gmail.com>
2023-12-13 22:13:38 +0545

    6 Day Complete

---
+++ Day 6/function.py
@@ -0,0 +1,11 @@
+# Built-in function:
+print("Hello")
+num_char = len("Bikash")
+
+
+# User-defined Function
+def my_function():
+    print("Hacker")
+    print("CyberAttack")
+
+my_function()
---
+++ Day 6/indentation.py
@@ -0,0 +1,11 @@
+#  Python uses indentation to signify the start and end of blocks.
+def example_function():
+    print("This is part of the function")
+    if True:
+        print("This is inside the if block")
+    else:
+        print("This is inside the else block")
+    print("This is still part of the function")
+
+# Function call
+example_function()
